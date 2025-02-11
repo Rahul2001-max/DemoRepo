22664a1600deb2a63bba75a6052fd683f1f7e5c6
Rahul Shankar Khatikamare <Rahul.Khatikamare@igtsolutions.com>
2025-02-11 21:47:39 +0530

    first commit

---
+++ Demo1.java
@@ -0,0 +1,21 @@
+class A{
+	A()
+	{
+		System.out.println("A");
+	}
+}
+
+class B{
+	B()
+	{
+		System.out.println("B");
+	}
+}
+class Demo1{
+	public static void main(String [] args)
+	{
+		System.out.println("Main");
+		A a = new A();
+		B b = new B();
+	}
+}
